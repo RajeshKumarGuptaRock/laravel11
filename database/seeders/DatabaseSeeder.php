0b97f4a69dd306bc1dc119f14c70358d5f4d087f
rajesh <developerrajeshgupta@gmail.com>
2024-11-02 19:58:31 +0530

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,13 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@user.com',
+        ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password1'),
+            'is_admin' => 1
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Middleware\UserMiddleware;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::get('/login',[AdminLoginController::class,'index']);
+    Route::post('/login',[AdminLoginController::class,'login'])->name('login');
+   
+    Route::middleware(['auth',AdminMiddleware::class])->group(function(){
+        Route::get('/home', [AdminHomeController::class, 'index'])->name('home');
+    });
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(UserMiddleware::class);
